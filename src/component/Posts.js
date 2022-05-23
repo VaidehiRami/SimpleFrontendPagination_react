0b906b4b47d2a52f69a82f1ec0f91040f20e73ac
VaidehiRami <ramivaidehi@gmail.com>
2022-05-23 16:09:53 +0530

    CHANGES

--- src/component/Posts.js
+++ src/component/Posts.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-const Posts = () => {
+const Posts = ({ posts, loading }) => {
+    if (loading) {
+        return <h2>Loading...</h2>;
+      }
   return (
-    <div>Posts</div>
-  )
+    <ul className='list-group mb-4'>
+      {posts.map(post => (
+        <li key={post.id} className='list-group-item'>
+          {post.title}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-export default Posts
+export default Posts;
